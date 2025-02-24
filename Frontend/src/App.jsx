47b72bd7ca7d3f55a8030b7bff7775742e0e75f4
Nikhil Patel <nikhilpatel26150@gmail.com>
2025-02-24 17:22:58 +0530

    pages updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Home from "./home/Home";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Courses from "./courses/Courses";
+import Signup from "./components/Signup";
+import { Toaster } from "react-hot-toast";
+import { useAuth } from "./context/AuthProvider";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [authUser, setAuthUser] = useAuth();
+  console.log(authUser);
   return (
     <>
-      hello
+      <div className="dark:bg-slate-900 dark:text-white">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/course"
+            element={authUser ? <Courses /> : <Navigate to="/signup" />}
+          />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+        <Toaster />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
